60d65829978d965f6d9b129266fa667e00bfd706
Ramiro Canillas <ramirocanillas31@gmail.com>
2022-07-26 12:09:57 -0300

    api

--- js/scripts.js
+++ js/scripts.js
@@ -137,3 +137,24 @@ window.onload = function(){
     renderCarrito()
   }
 }
+
+/* AJAX Y FETCH */
+
+const url = 'https://pokeapi.co/api/v2/pokemon/1'
+
+fetch(url)
+.then(response => response.json() )
+.then(data => {
+
+let element = document.getElementById('elem')
+  /*element.innerHTML = `
+  <p>${data.name}</p>
+  <p>${data.order}</p>
+  <img src= '${data.sprites.front_default}'/>
+  `;*/
+
+
+console.log(data)
+})
+
+.catch(err=>console.log(err))
